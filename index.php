ef5f9f3c8c9e6c160d26ec221d8b7a7fcd355d1c
AnttiAitta <n1aian00@students.oamk.fi>
2022-11-02 22:16:49 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+header('Content-Type: application/json');
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8','root','');
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $sql="select * from item";
+    $query = $db->query($sql);
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    header('HTTP/1.1 200 OK');
+    print json_encode($results,JSON_PRETTY_PRINT);
+} catch (PDOException $pdoex) {
+    header('HTTP1.1 500 Internal Server Error');
+    $error = array('error'=>$pdoex->getMessage());
+    print json_encode($error);
+}
